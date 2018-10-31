ed113ea1f7751089eecba9a4869119ab248a95b6
Hwa Min Jung <43151116+hwaminjung@users.noreply.github.com>
2018-10-30 20:52:07 -0700

    first

---
+++ FlailFIghters/Assets/newcontroller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newcontroller : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpHeight;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool grounded;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.x);
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.J))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.x);
+        }
+
+    }
+}
